3ac4ba279c5a72c605032053ae2fe8ffba78006e
Kenneth Felber <kefe@gfforsikring.dk>
2021-11-25 19:21:10 +0100

    added buffer overflow

---
+++ UCL.SoftwareSikkerhed/UCL.SoftwareSikkerhed.BufferOverflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace UCL.SoftwareSikkerhed.BufferOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new string[16];
+            var tmp = "Hep hest";
+            Console.WriteLine(tmp);
+            try
+            {
+                for (int i = 1; i <= 17; i++)
+                {
+                    array[i] = "hep hey";
+                }
+                Console.WriteLine(tmp);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
